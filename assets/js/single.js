6581b6283c1af4e4bc050eb6a7c0bf27b34d6b26
Ann-Marie <anniedillier@gmail.com>
2020-08-30 18:44:31 -0700

    single

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -9,16 +10,19 @@ var getRepoIssues = function(repo) {
           response.json().then(function(data) {
             displayIssues(data);
             console.log(data);
-          });
-        }
-        else {
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+                }
+            });
+        } else {
           alert("There was a problem with your request!");
         }
       });
 };
   
   getRepoIssues("facebook/react");
-  
+
   var displayIssues = function(issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
@@ -54,4 +58,16 @@ var getRepoIssues = function(repo) {
 
         issueContainerEl.appendChild(issueEl);
     }
+};
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+
 };
